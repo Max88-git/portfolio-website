4f1fee0a08d4a39564f4e90f9d09daebc25094a2
Max Lockwood <59417148+Max88-git@users.noreply.github.com>
2020-11-04 07:20:55 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function() {
+    $(".toggle").on("click", function(){
+        if($(".nav-item").hasClass("active")){
+            $(".nav-item").removeClass("active");
+        }
+        else{
+            $(".nav-item").addClass("active");
+        }
+    })
+});
